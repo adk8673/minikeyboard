4d587cc92b7cf423b6000ce6cd45927aed1bc9c9
adk8673 <adk8673@gmail.com>
2022-05-05 22:09:30 -0500

    Adding initial file

---
+++ MiniKeyboard.ino
@@ -0,0 +1,35 @@
+#include "HID-Project.h"
+
+#define D0 0
+#define D1 1
+#define D2 2
+#define D3 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+#define D8 8
+#define D9 9
+#define D10 10
+
+void setup() {
+  pinMode(D0, INPUT);
+  pinMode(D1, INPUT);
+  pinMode(D2, INPUT);
+  pinMode(D3, INPUT);
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(D6, INPUT);
+  pinMode(D7, INPUT);
+  pinMode(D8, INPUT);
+  pinMode(D9, INPUT);
+  pinMode(D10, INPUT);
+  Consumer.begin();
+}
+
+void loop() {
+  Consumer.write(MEDIA_VOLUME_UP);
+  delay(10000);
+  Consumer.write(MEDIA_VOLUME_DOWN);
+  delay(10000);
+}
