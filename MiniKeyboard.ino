8f9218fb25d703efd05d540ebe59d6cb9f179b2c
Alex Kane <adk8673@gmail.com>
2022-05-10 10:43:50 -0500

    Changed to use a keyboard matrix which is how the circuit board is set up

--- MiniKeyboard.ino
+++ MiniKeyboard.ino
@@ -1,74 +1,69 @@
 #include "HID-Project.h"
 
-#define D0 0
-#define D1 1
-#define D2 2
-#define D3 3
-#define D4 4
-#define D5 5
-#define D6 6
-#define D7 7
-#define D8 8
-#define D9 9
-#define D10 10
-#define D55 55
+byte rows[] = {2, 3, 4, 5};
+const int rowCount = sizeof(rows)/sizeof(rows[0]);
+
+// JP2 and JP3 are outputs
+byte cols[] = {8,9,10};
+const int colCount = sizeof(cols)/sizeof(cols[0]);
+
+byte keys[colCount][rowCount];
 
 void setup() {
-  pinMode(D0, INPUT_PULLUP);
-  pinMode(D1, INPUT_PULLUP);
-  pinMode(D2, INPUT_PULLUP);
-  pinMode(D3, INPUT_PULLUP);
-  pinMode(D4, INPUT_PULLUP);
-  pinMode(D5, INPUT_PULLUP);
-  pinMode(D6, INPUT_PULLUP);
-  pinMode(D7, INPUT_PULLUP);
-  pinMode(D8, INPUT_PULLUP);
-  pinMode(D9, INPUT_PULLUP);
-  pinMode(D10, INPUT_PULLUP);
-  Consumer.begin();
+  //Consumer.begin();
+  Serial.begin(115200);
+  while (!Serial) ; // wait for serial to start up
+  
+  for(int x=0; x<rowCount; x++) {
+    Serial.print(rows[x]); Serial.println(" as input");
+    pinMode(rows[x], INPUT);
+  }
+
+  for (int x=0; x<colCount; x++) {
+    Serial.print(cols[x]); Serial.println(" as input-pullup");
+    pinMode(cols[x], INPUT_PULLUP);
+  }
+    
+}
+
+void readMatrix() {
+  // iterate the columns
+  for (int colIndex=0; colIndex < colCount; colIndex++) {
+    // col: set to output to low
+    byte curCol = cols[colIndex];
+    pinMode(curCol, OUTPUT);
+    digitalWrite(curCol, LOW);
+
+    // row: interate through the rows
+    for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+      byte rowCol = rows[rowIndex];
+      pinMode(rowCol, INPUT_PULLUP);
+      keys[colIndex][rowIndex] = digitalRead(rowCol);
+      pinMode(rowCol, INPUT);
+    }
+    // disable the column
+    pinMode(curCol, INPUT);
+  }
+}
+
+void printMatrix() {
+  for (int rowIndex=0; rowIndex < rowCount; rowIndex++) {
+    if (rowIndex < 10)
+      Serial.print(F("0"));
+    Serial.print(rowIndex); Serial.print(F(": "));
+
+    for (int colIndex=0; colIndex < colCount; colIndex++) { 
+      Serial.print(keys[colIndex][rowIndex]);
+      if (colIndex < colCount)
+        Serial.print(F(", "));
+    } 
+    Serial.println("");
+  }
+  Serial.println("");
 }
 
 void loop() {
-  //Consumer.write(MEDIA_VOLUME_UP);
-  //Serial.println("test1");
-  if (digitalRead(D0) == LOW)
-  {
-     Serial.println("D0");
-  }
-  if (digitalRead(D1) == LOW)
-  {
-     Serial.println("D1");
-  }
-  if (digitalRead(D2) == LOW)
-  {
-     Serial.println("D2");
-  }
-  if (digitalRead(D3) == LOW)
-  {
-     Serial.println("D3");
-  }
-  if (digitalRead(D4) == LOW)
-  {
-     Serial.println("D4");
-  }
-  if (digitalRead(D5) == LOW)
-  {
-     Serial.println("D5");
-  }
-  if (digitalRead(D6) == LOW)
-  {
-     Serial.println("D6");
-  }
-  if (digitalRead(D7) == LOW)
-  {
-     Serial.println("D7");
-  }
-  if (digitalRead(D8) == LOW)
-  {
-     Serial.println("D8");
-  }
-  //Serial.println("test2");
-  //delay(2000);
-  //Consumer.write(MEDIA_VOLUME_DOWN);
-  //delay(2000);
+  readMatrix();
+  if (Serial.read()=='!')
+    printMatrix();
 }
